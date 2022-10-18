296868478dac3353bc7e194449b1b1ecf30cfb25
onokajames96 <jamesonoka996@gmail.com>
2022-10-18 09:16:08 -0700

    Fixing betty doc

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - Entry point prints size of data type in C
+ * main - Entry point
  *
  * return: (0)
  */
@@ -14,8 +14,8 @@ float f;
 
 printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
-printf("Size of a long int: %lu byte\n", (unsigned long)sizeof(c));
-printf("Size of a long long int: %lu byte\n", (unsigned long)sizeof(d));
-printf("Size of a float: %lu byte\n", (unsigned long)sizeof(f));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 return (0);
 }
