4f7f8657903be853d9b6d319894932e0d6f8f75d
onokajames96 <jamesonoka996@gmail.com>
2022-10-18 07:50:03 -0700

    my first line in C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 /**
- *
  *  * main - Entry point
  *
  *   *
@@ -11,6 +10,6 @@
 int main(void)
 {
 
-printf("with proper grammar, but the outcome is a piece of art");
+printf("with proper grammar, but the outcome is a piece of art,");
 	return (0);
 }
