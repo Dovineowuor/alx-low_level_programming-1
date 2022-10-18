f5fc32e9234044f16b711f7938fab7f314d41a83
onokajames96 <jamesonoka996@gmail.com>
2022-10-18 07:30:49 -0700

    Debug

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
-/*
- * main -prints a line of code using puts function
+/**
+ * main - Entry point prints a line of code using puts function
  *
- * Return: returns 0
-*/
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
