4cb6a1c44cc383d7af8346045218492fe9409c02
onoka james <99639720+onokajames96@users.noreply.github.com>
2022-10-21 13:08:31 +0300

    fix betty linter

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,8 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
-/* more headers goes there */
-
+/* more headers goes there*/
+/*
+* main - Entry point returning the outputs
+* Return: Always return 0 (Success)
+*/
 /* betty style doc for function main goes there */
 int main(void)
 {
